65688d68ab8a7573226b04ca8432a74ceed2789e
Andrei Bondaret <102138730+Bonda3Vaax@users.noreply.github.com>
2023-10-29 21:59:43 +1100

    Delete assets/js directory

--- assets/js/scripts.js
+++
@@ -1,4 +0,0 @@
-const button = document.getElementsByClassName('button')[0]
-const navlinks = document.getElementsByClassName('navlinks')[0]
-
-button.addEventListener('click', () => {navlinks.classList.toggle('active')})
